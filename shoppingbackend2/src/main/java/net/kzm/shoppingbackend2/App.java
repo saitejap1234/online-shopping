38c28678d0d2a2e9590083c7f795a84f9c862ba0
saiteja <dd@gmail.com>
2020-10-15 19:24:35 +0530

    my proj

---
+++ shoppingbackend2/src/main/java/net/kzm/shoppingbackend2/App.java
@@ -0,0 +1,13 @@
+package net.kzm.shoppingbackend2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
